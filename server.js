ebe92a04928c20cf24443dfe69ad7c81d9579b9a
bennyatoffice <bennyatoffice@gmail.com>
2020-01-03 10:03:52 +0530

    serverfile

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/basketlegend'));
+app.use(express.static(__dirname + '/dist/BasketLegend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/basketlegend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/BasketLegend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
